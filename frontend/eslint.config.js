690b5994f7da5ad13f137b7481548c6207a0f80f
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-06-03 18:19:39 -0400

    vitest capability added

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import vitestGlobals from "vitest/globals"
 
 export default [
   { ignores: ['dist'] },
@@ -9,7 +10,8 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      // Add globals for both browser and vitest here
+      globals: { ...globals.browser, ...vitestGlobals },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
