ec4125acfeafff4db2c289ee3ace5ee39bce9474
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-06-11 03:06:29 -0400

    msw handlers and server moved to __mocks__

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,7 +1,7 @@
 import { expect, afterEach, afterAll, beforeAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers"
-import { server } from "./mocks/server";
+import { server } from "./__mocks__/server";
 
 expect.extend(matchers)
 
