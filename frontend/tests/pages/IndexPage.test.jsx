cddf602e8198d0433a28813a1908f3daeebe86e1
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-07-12 04:46:05 -0400

    test for alt text

--- frontend/tests/pages/IndexPage.test.jsx
+++ frontend/tests/pages/IndexPage.test.jsx
@@ -5,27 +5,39 @@ import IndexPage from '../../src/pages/IndexPage';
 import { TestUserProvider } from '../providers';
 import { BASE_URL } from '../../settings';
 import { server } from '../__mocks__/server';
+import { expect } from 'vitest';
 
 describe('IndexPage', () => {
-  const renderCompnent = () => render(
+  const renderCompnent = () =>
+    render(
       <TestUserProvider>
         <IndexPage />
       </TestUserProvider>,
-    )
+    );
   it('renders', async () => {
     renderCompnent();
 
     await waitFor(() => {
-      expect(screen.getAllByTestId("imagecard")).toHaveLength(4);
+      expect(screen.getAllByTestId('imagecard')).toHaveLength(4);
       expect(screen.getByTestId('imagecardcontainer')).toBeInTheDocument();
     });
   });
 
+  it('has alt text for all images', async () => {
+    renderCompnent();
+
+    await waitFor(() => {
+      const images = screen.getAllByRole('img');
+
+      images.forEach((image) => {
+        expect(image).toHaveAccessibleName();
+      });
+    });
+  });
+
   it('displays error when error status', async () => {
-    server.use(
-      http.get(`${BASE_URL}`, async () => HttpResponse.error()),
-    );
-    renderCompnent()
+    server.use(http.get(`${BASE_URL}`, async () => HttpResponse.error()));
+    renderCompnent();
 
     await waitFor(() => {
       expect(
