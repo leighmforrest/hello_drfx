05c70c36e496b69b8907c54d7e2f83bea1d47e4f
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-07-15 18:29:45 -0400

    picture upload page ui improved

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spinner = () => (
   <div
-    className="animate-spin h-6 w-6 border-t-2 border-white rounded-full"
+    className="animate-spin h-96 w-96 border-t-2 border-white rounded-full"
     data-testid="spinner"
   />
 );
--- frontend/src/pages/NewPicturePage.jsx
+++ frontend/src/pages/NewPicturePage.jsx
@@ -5,28 +5,34 @@ import MainContainer from '../components/MainContainer';
 import { useCreatePictureMutation } from '../mutations/createPictureMutation';
 import { useNavigate } from 'react-router';
 import Spinner from '../components/Spinner';
+import { useState } from 'react';
 
 const NewPicturePage = () => {
+  const [showSpinner, setShowSpinner] = useState(false);
   const { mutate, isLoading, isError } = useCreatePictureMutation();
   const navigate = useNavigate();
 
   const submitHandler = async ({ title, picture }) => {
     const file = picture[0];
+    setShowSpinner(true);
     if (file) {
       mutate(
         { title, picture: file },
         {
           onSuccess: () => {
             toast('The picture has been uploaded.');
-            navigate('/');
+            setTimeout(() => {
+              navigate('/');
+            }, 500);
           },
-          onError: () => toast.error('The image could not be uploaded.'),
+          onError: () => {
+            toast.error('The image could not be uploaded.');
+            setShowSpinner(false);
+          },
         },
       );
     }
   };
-
-  if (isLoading) return <h2>Loading...</h2>;
 
   if (isError) return <h1>Unable. Malfunction. Need input.</h1>;
 
@@ -34,7 +40,13 @@ const NewPicturePage = () => {
     <MainContainer>
       <section className="min-h-[calc(100vh-7rem)] flex items-center justify-center px-4">
         <div className="w-full px-2 sm:max-w-3xl sm:px:0">
-          {isLoading ?  <Spinner/> :(<PictureForm onUpload={submitHandler} />)}
+          {isLoading || showSpinner ? (
+            <div className="flex justify-center items-center h-40">
+              <Spinner />
+            </div>
+          ) : (
+            <PictureForm onUpload={submitHandler} />
+          )}
         </div>
       </section>
     </MainContainer>
